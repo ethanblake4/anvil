07dcd808358b9eab79773338e75bb8d4d0b4feeb
Ethan <ethanblake@gmail.com>
2021-07-05 23:42:32 -0600

    pub release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+* Fix links
+
 ## 0.1.0
 
 * Initial release.
---
+++ bin/anvil.dart
@@ -0,0 +1,5 @@
+import 'package:anvil/anvil.dart';
+
+void main(List<String> args) => anvil(args);
+
+final anvil = Anvil();
